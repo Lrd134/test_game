c6cfc009cfc6fea7c1dc9c0841c58a9267781ceb
yashpatel0288 <94777303+yashpatel0288@users.noreply.github.com>
2021-11-21 13:24:01 +0530

    reference to rigidbody

--- Assets/scripts/player/PlayerMovement.cs
+++ Assets/scripts/player/PlayerMovement.cs
@@ -2,13 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerMovement : MonoBehaviour {
+    public Rigibody rb;
 
     // Update is called once per frame
     void Update()
